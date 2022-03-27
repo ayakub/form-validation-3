b13f15cf4ca32102d94d269e63a67ffcb7a94007
ayakub <79369658+ayakub@users.noreply.github.com>
2022-03-27 21:00:43 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function ValidateEmail() {
-    var email = document.getElementById("txtEmail").value;
+    var form = document.getElementById('form');
+    var email = document.getElementById("txtEmail");
     var lblError = document.getElementById("lblError");
     lblError.innerHTML = "";
     var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
